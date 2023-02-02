8d3dcd80c6a0046c356851316a08ee8c1169fb1e
tktoma <thomassaldias1@gmail.com>
2023-02-02 12:00:37 +0100

    a

---
+++ pendu + nbr alea/bataille_navalle.cpp
@@ -0,0 +1,1 @@
+#include "bataille_navalle.h"
--- pendu + nbr alea/main.cpp
+++ pendu + nbr alea/main.cpp
@@ -1,8 +1,11 @@
 #include "jeu_pendu.h"
 #include "nbr_aleatoire.h"
+#include "cadrillage.h"
 int main()
 {
 	jeu_pendu jeu;
 	nbr_aleatoire jeu2;
-	jeu2.lancement();
+	cadrillage ui(0,0);
+	ui.cadreaffiche();
+
 }
